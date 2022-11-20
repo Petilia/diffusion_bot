2591a3289a07ad88fd5ef1c6f37c6d6fc96627f6
m-kichik <kichik.mg@phystech.edu>
2022-11-21 00:54:28 +0300

    final

--- diffusion_bot/bot.py
+++ diffusion_bot/bot.py
@@ -5,6 +5,7 @@ import redis
 from diffusers import StableDiffusionPipeline
 from huggingface_hub.hf_api import HfFolder
 from dotenv import load_dotenv
+import time
 
 load_dotenv()
 HfFolder.save_token(os.getenv("HF_TOKEN"))
@@ -12,20 +13,22 @@ HfFolder.save_token(os.getenv("HF_TOKEN"))
 MODEL_DATA = os.getenv('MODEL_DATA')
 LOW_VRAM_MODE = (os.getenv('LOW_VRAM', 'true').lower() == 'true')
 USE_AUTH_TOKEN = (os.getenv('USE_AUTH_TOKEN', 'true').lower() == 'true')
+INFERENCE_DEVICE = os.getenv('INFERENCE_DEVICE')
 
 # load the text2img pipeline
 revision = "fp16" if LOW_VRAM_MODE else None
 torch_dtype = torch.float16 if LOW_VRAM_MODE else None
 pipe = StableDiffusionPipeline.from_pretrained(MODEL_DATA, revision=revision, 
                                                 torch_dtype=torch_dtype, use_auth_token=USE_AUTH_TOKEN)
-pipe = pipe.to("cuda")
+pipe = pipe.to(INFERENCE_DEVICE)
 
 #redis 
-r = redis.Redis(
+redis_db = redis.Redis(
     host="redis", 
     password="password"
 )
 
+#TG-bot
 bot=telebot.TeleBot(os.getenv("TG_TOKEN"))
 
 @bot.message_handler(commands=['start'])
@@ -42,8 +45,7 @@ def generation_image_by_prompt(message):
     prompt = message.text
     print(prompt)
     image = pipe(prompt).images[0]
-
+    redis_db.set(str(time.time()), prompt) # save prompt
     bot.send_photo(message.from_user.id, image)
-
 
 bot.infinity_polling()
